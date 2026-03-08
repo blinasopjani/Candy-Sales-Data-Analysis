7d8e458eeb01d02addb92329563da8498202481c
blinasopjani <115405101+blinasopjani@users.noreply.github.com>
2026-03-08 12:37:40 +0100

    Add files via upload

---
+++ sales_db_query.sql
@@ -0,0 +1,29 @@
+
+CREATE TABLE "Sales" (
+    "Product ID" INT,
+    "Division" TEXT,
+    "Gross Profit" FLOAT
+);
+
+
+CREATE TABLE "Products" (
+    "Product ID" INT,
+    "Product Name" TEXT
+);
+
+
+CREATE TABLE "Targets" (
+    "Division" TEXT,
+    "Target" FLOAT
+);
+
+CREATE TABLE "US Zips" (
+    "Zip" TEXT,
+    "City" TEXT
+);
+
+CREATE TABLE "Factories" (
+    "Factory ID" INT,
+    "Factory Name" TEXT
+);
+
